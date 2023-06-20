ba55d1b2041a58e952488ba18987712e95ee3de0
HarshithRajesh <harshithr2004@gmail.com>
2023-06-20 19:21:55 +0530

    some changes

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from fpdf import FPDF
 pdf = FPDF(orientation='P',unit='pt',format='A4')
 pdf.add_page()
 pdf.set_font(family='Times', style='B',size=36)
-pdf.cell(w=0,h=0,txt="AI FUTURE",align='C',ln=2)
+pdf.cell(w=0,h=15,txt="AI FUTURE",align='C',ln=2)
 pdf.image('ai.jpg',w=200,h=180,x=50,y=50)
 pdf.set_font(family='Times',style='B',size=24)
-pdf.cell(w=0,h=0,txt="AI is gonna change the future",ln=1)
+
+pdf.cell(w=10,h=400,txt="AI is gonna change the future")
 
 
 pdf.output('first_pdf.pdf')
